f7534b8f58def05785894c1c9cb701996bcfdbfd
UtarshM <utarsh@example.com>
2026-03-05 00:20:32 +0530

    chore: revert local migration due to remote mismatch

--- supabase/migrations/20260305000000_update_free_credits.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.handle_new_user_credits()
-RETURNS TRIGGER AS $$
-BEGIN
-  INSERT INTO public.workspace_credits (user_id, total_credits)
-  VALUES (NEW.id, 5); -- Give 5 free credits on sign up (DOWN FROM 50)
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
